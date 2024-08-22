ba4362d9c5f8b7a4c7872566d0bd7489f131c21b
Beernaert Robbe <robbe.beernaert@student.howest.be>
2024-08-23 00:20:02 +0200

    added explanation as to the general workings of the streamWriter

--- Athena/src/streams/writers/streamWriter.h
+++ Athena/src/streams/writers/streamWriter.h
@@ -3,7 +3,11 @@
 #define STREAM_WRITER
 #include <vector>
 namespace luna 
-{
+{	/** 
+	* @brief this is the interface for all streamWriters
+	* sections is the collection of uncompressed athena::buffer objects to write to!
+	* when the streamWriter is flushed all of them get transformed into athena::compressedBuffer objects.
+	*/
 	class streamWriter
 	{
 	public:
@@ -12,7 +16,7 @@ namespace luna
 		virtual void setStreamPosition();
 		virtual void previousStreamSection();
 		virtual void nextStreamSection();
-		virtual size_t getStreamSectionCount();
+		virtual size_t getStreamSectionCount() const;
 		virtual bool writeData(const char* data, size_t size);
 		template<typename T>
 		void writeObject(const T& object) {};
